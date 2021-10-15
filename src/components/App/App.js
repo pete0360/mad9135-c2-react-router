a7eeb8710cd57ed103c4765982207faa8e21c520
bpeterkins28 <ben.peterkins@gmail.com>
2021-10-15 03:51:43 -0400

    added routes to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from "../Navbar/Navbar"
+import People from "../People/People"
+import Not from "../Not/Not"
+import Home from "../Home/Home"
+import { Switch, Route, Redirect } from 'react-router-rom'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="App">
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <Switch>
+        <Route path="/people">
+        <People />
+        </Route>
+        <Route path="/404">
+        <Not />
+        </Route>
+        <Route 
+        path="/"
+        exact>
+        <Home />
+        </Route>
+        <Redirect to = "/404" />
+      </Switch>
+    </main>
+  </div>
 }
 
 export default App;
